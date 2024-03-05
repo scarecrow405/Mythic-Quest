e1ff333428bb9130a62e69f7bce173049cafe940
scarecrow405 <scarecrow.theproducer@gmail.com>
2024-03-05 02:33:53 +0200

    "Fixed Template Tag Register"

--- monsters/templatetags/custom_filters.py
+++ monsters/templatetags/custom_filters.py
@@ -1,8 +1,8 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='first_word')
-def first_word(value):
-    return value.split(" ")[0] if value else ''
+# from django import template
+#
+# register = template.Library()
+#
+#
+# @register.filter(name='first_word')
+# def first_word(value):
+#     return value.split(" ")[0] if value else ''
